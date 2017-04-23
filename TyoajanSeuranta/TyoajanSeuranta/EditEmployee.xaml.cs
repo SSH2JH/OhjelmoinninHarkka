cc055f385fefd2fc36b3cbb489e9d2df2590caa5
Juuso Huppunen <K8960@a.a>
2017-04-23 18:08:27 +0300

    I've added new windows and some basic functionality.

--- TyoajanSeuranta/TyoajanSeuranta/EditEmployee.xaml.cs
+++ TyoajanSeuranta/TyoajanSeuranta/EditEmployee.xaml.cs
@@ -10,26 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TyoajanSeuranta {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for EditEmployee.xaml
 	/// </summary>
-	public partial class MainWindow : Window {
-		public MainWindow()
+	public partial class EditEmployee : Window {
+		public EditEmployee()
 		{
 			InitializeComponent();
-		}
-
-		private void SignOut_Click(object sender, RoutedEventArgs e)
-		{
-			SignIn next = new SignIn();
-			next.ShowDialog();
-			if (string.IsNullOrEmpty(Properties.Settings.Default.UserPin) == false) {
-				sp_MainControls.IsEnabled = true;
-			}
 		}
 	}
 }
---
+++ TyoajanSeuranta/TyoajanSeuranta/SignIn.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TyoajanSeuranta {
+	/// <summary>
+	/// Interaction logic for SignIn.xaml
+	/// </summary>
+	public partial class SignIn : Window {
+		public SignIn()
+		{
+			InitializeComponent();
+		}
+
+		private void button_Cancel_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void button_Ok_Click(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrEmpty(txt_PinCode.Text) == true) {
+				txt_signinMessages.Text = "Pin koodi on pakollinen!";
+			} else {
+				Properties.Settings.Default.UserPin = txt_PinCode.Text;
+				txt_signinMessages.Text = "Tervetuloa!";
+				this.Close();
+			}
+		}
+	}
+}
